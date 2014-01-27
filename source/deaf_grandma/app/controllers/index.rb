d14ba63df3324b9080a5866e304f0a608f8784a8
Emmanuel Kaunitz & John Olmsted <pair+mechamanny.qsymmachus@devbootcamp.com>
2014-01-27 15:23:28 -0800

    Added redirects to index.rb; Granny talks now!

--- source/deaf_grandma/app/controllers/index.rb
+++ source/deaf_grandma/app/controllers/index.rb
@@ -1,9 +1,15 @@
 get '/' do
-  @grandma = params[:grandma]
-  # Look in app/views/index.erb
-  erb :index
+	@grandma = params[:grandma]
+	# Look in app/views/index.erb
+	erb :index
 end
 
 post '/grandma' do
-  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+	input = params[:user_input]
+	if input == input.upcase
+		redirect to ('/?grandma=NO, NOT SINCE 1945!')
+	else
+		redirect to ('/?grandma=HUH? SPEAK UP SONNY')
+	end
 end
+
