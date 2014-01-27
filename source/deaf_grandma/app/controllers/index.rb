81d3aaf20814d1634decaab6cb188155f147f01c
Matthew Higgins & Patrick Vilhena <pair+mh120888.nifengle@devbootcamp.com>
2014-01-27 15:10:58 -0800

    Creates /grandma route with logic and redirect to /?grandma=#{response}

--- source/deaf_grandma/app/controllers/index.rb
+++ source/deaf_grandma/app/controllers/index.rb
@@ -5,5 +5,9 @@ get '/' do
 end
 
 post '/grandma' do
-  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+  grandmaisms = ["When did we start letting the Catholics in?",
+    "Careful, those gypsies will curse you!", "Damn Canadians and their beady eyes!",
+    "You know what they say about Aleutians...", "No daughter of mine is gonna marry a San Marinan haberdasher!"]
+  response = params[:user_input].upcase == params[:user_input] ? grandmaisms.sample : "Speak up, sonny!"
+  redirect "/?grandma=#{response}"
 end
