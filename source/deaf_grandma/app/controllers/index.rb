1a1905fc3771b24098d01c8a528ecf863e8e5df4
Spencer Smitherman & Christina Taggart <pair+bigspencey.christina-taggart@devbootcamp.com>
2014-01-27 15:40:51 -0800

    Got Grandma to speak

--- source/deaf_grandma/app/controllers/index.rb
+++ source/deaf_grandma/app/controllers/index.rb
@@ -1,3 +1,7 @@
+require 'sinatra'
+
+enable :sessions
+
 get '/' do
   @grandma = params[:grandma]
   # Look in app/views/index.erb
@@ -6,4 +10,11 @@ end
 
 post '/grandma' do
   "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+  input = params[:user_input]
+  if input == input.upcase
+    response = "Hey Sonny, it's great to see your face in the retirement home."
+  else
+    response = "Speak up Sonny!!!"
+  end
+  redirect "/?grandma=#{response}"
 end
