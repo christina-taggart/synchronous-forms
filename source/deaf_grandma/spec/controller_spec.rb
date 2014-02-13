a8d3c9eb3526ff18b644a7ae9821d97f3580d9ca
John Olmsted <johnaolmsted@gmail.com>
2014-02-13 15:38:43 -0800

    Adds simple controller tests

--- source/deaf_grandma/spec/controller_spec.rb
+++ source/deaf_grandma/spec/controller_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
 
-describe "Index controller" do
+describe "index controller" do
   it "should get the index" do
     get "/"
     expect(last_response).to be_ok
+  end
+end
+
+describe "grandma's controller" do
+  it "should redirect with 'Speak up, Sonny!' if user input is lower case" do
+    user_input = "hello"
+    params = { user_input: user_input }
+    post("/grandma", params)
+    expect(last_response.location.include?("Speak up, sonny!"))
+  end
+
+  it "should redirect with random phrase if user input is upper case" do
+    user_input = "HELLO"
+    params = { user_input: user_input }
+    post("/grandma", params)
+    expect(last_response.location.include?("Speak up, sonny!")).to eq false
   end
 end
